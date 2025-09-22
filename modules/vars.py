c07cecbe0af7fa9dabaae36ad9e3e8cb9da068f5
xPrafulx143 <pramodjamnik100@gmail.com>
2025-09-22 07:08:34 +0530

    Add files via upload

---
+++ modules/vars.py
@@ -0,0 +1,26 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "3140089"))
+API_HASH = environ.get("API_HASH", "ef67c500cfbd85b764535cf1c8c9917f")
+BOT_TOKEN = environ.get("7087365550:AAG28T3GguuquYNAftyN0BW_LaK3UxeFPZM", "")
+
+OWNER = int(environ.get("OWNER", "7361052650"))
+CREDIT = environ.get("CREDIT", "PRUNUS💋")
+cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
+
+TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
+
+AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
+  
+# .....,.....,.......,...,.......,....., .....,.....,.......,...,.......,.....,
+api_url = "http://master-api-v3.vercel.app/"
+api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
+# .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
